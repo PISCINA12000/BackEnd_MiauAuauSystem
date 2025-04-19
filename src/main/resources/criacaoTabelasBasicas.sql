b74d7c6f21df09f54d3f20be5bc3161ef0fc2479
matheusmen1 <antonuccimatheus1@outlook.com>
2025-04-18 22:15:46 -0300

    identação

--- src/main/resources/criacaoTabelasBasicas.sql
+++ src/main/resources/criacaoTabelasBasicas.sql
@@ -34,11 +34,11 @@ CREATE TABLE usuario (
     usu_numero VARCHAR(10) NOT NULL
 );
 CREATE TABLE adocao (
-                        ado_id SERIAL PRIMARY KEY,
-                        ado_animal_id INTEGER NOT NULL,
-                        ado_usuario_id INTEGER NOT NULL,
-                        ado_data DATE NOT NULL,
-                        ado_status VARCHAR(50) NOT NULL,
-                        FOREIGN KEY (ado_animal_id) REFERENCES animal(ani_id),
-                        FOREIGN KEY (ado_usuario_id) REFERENCES usuario(usu_id)
+    ado_id SERIAL PRIMARY KEY,
+    ado_animal_id INTEGER NOT NULL,
+    ado_usuario_id INTEGER NOT NULL,
+    ado_data DATE NOT NULL,
+    ado_status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (ado_animal_id) REFERENCES animal(ani_id),
+    FOREIGN KEY (ado_usuario_id) REFERENCES usuario(usu_id)
 );
